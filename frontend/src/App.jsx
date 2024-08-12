ac385150e9519c792f629f6a1d33fe082ad61250
himanshugaupale <himanshugaupale875@gmail.com>
2024-08-12 17:20:32 +0530

    commit for navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createBrowserRouter } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/shared/Navbar'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/signup",
+    element: <Signup/>
+  }
+
+])
+function App(){
 
   return (
     <>
+      <h1>This is front end part</h1>
       <Navbar/>
-      <h1>this is frontend dashboard</h1>
     </>
   )
 }
 
-export default App
+export default App 
