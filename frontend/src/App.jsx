f8cf90cb4bf168992d1bd014b54bfc6f9672e017
himanshugaupale <himanshugaupale875@gmail.com>
2024-08-19 15:26:18 +0530

    commited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Navbar from './components/shared/Navbar'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
+import Home from './components/Home'
+
 
 
 
